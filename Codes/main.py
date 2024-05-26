6ab64c1c53eb031bf97d0469d80c1113868b52dc
Dako <120756724+Damir-Kanymkulov@users.noreply.github.com>
2024-05-26 16:07:49 +0200

    Update main.py
    I added brute_force_search() to the main part
    
    Signed-off-by: Dako <120756724+Damir-Kanymkulov@users.noreply.github.com>

--- Codes/main.py
+++ Codes/main.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 from Dataset import img_dim, attr_dim, train_image_loader, train_attr_loader, val_image_loader, val_attr_loader
 from late_fusion import late_fusion
 from intermediate_fusion_GA import interm_fusion_GA
+from brute_force_search import brute_force_search
 
 #-----------------------------------------------------Fusion results------------------------------------------------------------------
 if __name__ == "__main__":
@@ -15,9 +16,11 @@ if __name__ == "__main__":
     num_generations = 1
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #Training fusion models
-    late_fusion_loss = late_fusion(attr_dim, img_dim, train_attr_loader, train_image_loader, val_attr_loader, val_image_loader, device, lr, num_epochs, criterion)
+    brute_force_loss, solution0, solution1 = brute_force_search(attr_dim, img_dim, train_attr_loader, train_image_loader, val_attr_loader, val_image_loader, device, lr, num_epochs, criterion)
     solution_GA, interm_fusion_loss_GA = interm_fusion_GA(attr_dim, img_dim, train_attr_loader, train_image_loader, val_attr_loader, val_image_loader, device, lr, num_epochs, num_generations, criterion)
+    late_fusion_loss = late_fusion(attr_dim, img_dim, train_attr_loader, train_image_loader, val_attr_loader, val_image_loader, device, lr, num_epochs, criterion)
     print("-------------------------------------Results-----------------------------------------------------")
-    print("Late fusion loss: {:.2f}".format(late_fusion_loss))
-    print("Intermediate fusion loss (GA optimization): {:.2f} \t Solution: {}".format(interm_fusion_loss_GA, solution_GA))    
+    print("Brute force search loss: {:.4f} \t Best combination of NN layers: {}".format(brute_force_loss, [solution0, solution1]))
+    print("Intermediate fusion loss (GA optimization): {:.4f} \t Best combination of NN layers: {}".format(1/(interm_fusion_loss_GA), solution_GA))    
+    print("Late fusion loss: {:.4f}".format(late_fusion_loss))  
     print("-------------------------------------------------------------------------------------------------")
