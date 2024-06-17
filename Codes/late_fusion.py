3bf047a96cfda15e04ccdb1a9e561b2adf2c96c5
Dako <120756724+Damir-Kanymkulov@users.noreply.github.com>
2024-06-17 19:56:09 +0200

    Update late_fusion.py
    Signed-off-by: Dako <120756724+Damir-Kanymkulov@users.noreply.github.com>

--- Codes/late_fusion.py
+++ Codes/late_fusion.py
@@ -7,18 +7,17 @@ import numpy as np
 from early_fusion import MLP, train
 
 #----------------------------------------------------Late fusion---------------------------------------------------------------
-def late_fusion(attr_dim, img_dim, train_attr_loader, train_image_loader, val_attr_loader, val_image_loader, device, lr=0.01, num_epochs=1, criterion=nn.L1Loss()):
-    mlp_num = MLP(input_dim=attr_dim, fusion="late")
-    mlp_img = MLP(input_dim=img_dim, fusion="late")
-    optimizer1 = optim.Adam(mlp_num.parameters(), lr=lr)
-    optimizer2 = optim.Adam(mlp_img.parameters(), lr=lr)
-    #Training
-    mlp_num_path = 'best_num_late_fusion_model_min_val_loss.pth'
-    mlp_img_path = 'best_img_late_fusion_model_min_val_loss.pth'
-    num_dict_log = train(mlp_num, optimizer1, num_epochs, train_attr_loader, val_attr_loader, criterion, device, mlp_num_path)
-    img_dict_log = train(mlp_img, optimizer2, num_epochs, train_image_loader, val_image_loader, criterion, device, mlp_img_path)
+def late_fusion(dimension_dict, loaders_dict, device, lr=0.01, num_epochs=1, criterion=nn.L1Loss()):
+    loss_array = []
+    for data_type in loaders_dict.keys():
+        for i, (train_loader, val_loader) in enumerate(loaders_dict[data_type]):
+            model = MLP(input_dim=dimension_dict[data_type], fusion="late")
+            optimizer = optim.Adam(model.parameters(), lr=lr)
+            #Training
+            model_path = 'best_late_fusion_model_' + data_type + '_' + str(i) + '.pth'
+            num_dict_log = train(model, optimizer, num_epochs, train_loader, val_loader, criterion, device, model_path)
+            checkpoint = torch.load(model_path)
+            loss_array.append(checkpoint['loss'])
     #Results of late fusion
-    checkpoint1 = torch.load('best_num_late_fusion_model_min_val_loss.pth')
-    checkpoint2 = torch.load('best_img_late_fusion_model_min_val_loss.pth')
-    loss = (checkpoint1['loss'] + checkpoint2['loss'])/2
+    loss = sum(loss_array) / len(loss_array)
     return loss
