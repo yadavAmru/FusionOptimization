d6462564dfcd6dd1faa8b8e0a556c5b2f229c73d
Dako <120756724+Damir-Kanymkulov@users.noreply.github.com>
2024-07-04 20:47:00 +0200

    Update late_fusion.py
    Signed-off-by: Dako <120756724+Damir-Kanymkulov@users.noreply.github.com>

--- Codes/Late_fusion/late_fusion.py
+++ Codes/Late_fusion/late_fusion.py
@@ -4,20 +4,23 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
-from early_fusion import MLP, train
+from early_fusion import MLP, train, validate, save_model, load_model
 
 #----------------------------------------------------Late fusion---------------------------------------------------------------
 def late_fusion(dimension_dict, loaders_dict, device, lr=0.01, num_epochs=1, criterion=nn.L1Loss()):
     loss_array = []
-    for data_type in loaders_dict.keys():
-        for i, (train_loader, val_loader) in enumerate(loaders_dict[data_type]):
+    dict_log = {}
+    for data_type in dimension_dict.keys():
+        for i, (train_loader, val_loader, test_loader) in enumerate(zip(loaders_dict["train"][data_type], loaders_dict["val"][data_type], loaders_dict["test"][data_type])):
             model = MLP(input_dim=dimension_dict[data_type], fusion="late")
             optimizer = optim.Adam(model.parameters(), lr=lr)
-            #Training
             model_path = 'best_late_fusion_model_' + data_type + '_' + str(i) + '.pth'
-            num_dict_log = train(model, optimizer, num_epochs, train_loader, val_loader, criterion, device, model_path)
-            checkpoint = torch.load(model_path)
-            loss_array.append(checkpoint['loss'])
-    #Results of late fusion
-    loss = sum(loss_array) / len(loss_array)
-    return loss
+            log = train(model, optimizer, num_epochs, train_loader, val_loader, criterion, device, model_path)   #train model with validation
+            dict_log[data_type] = log
+            # checkpoint = torch.load(model_path)
+            # loss_array.append(checkpoint['loss'])                               #Collect all validation losses for each modality
+            test_model = load_model(model, model_path)
+            test_loss = validate(test_model, test_loader, criterion, device)
+            loss_array.append(test_loss)                                        #Collect all test losses for each modality
+    test_loss = sum(loss_array) / len(loss_array)                               #Find mean value fo all validation results to get final loss for late fusion
+    return test_loss, dict_log
