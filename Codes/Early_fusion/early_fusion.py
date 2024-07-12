ba67f32551db9b8955749a58b139f44f5969dc79
Dako <120756724+Damir-Kanymkulov@users.noreply.github.com>
2024-07-12 02:45:22 +0200

    Update early_fusion.py
    Signed-off-by: Dako <120756724+Damir-Kanymkulov@users.noreply.github.com>

--- Codes/Early_fusion/early_fusion.py
+++ Codes/Early_fusion/early_fusion.py
@@ -9,22 +9,22 @@ import numpy as np
 #-----------------------------------------------------Definition of functions-----------------------------------------------------
 #MLP model flexible structure
 class MLP(nn.Module):
-    def __init__(self, input_dim, fusion, n_layers=None, output_dim=1):
+    def __init__(self, input_dim, fusion, mode, n_layers=None, output_dim=1):
         super(MLP, self).__init__()
         self.input_dim = input_dim
         self.fusion = fusion
+        self.mode = mode
         self.output_dim = output_dim
-        if n_layers == None:                # if we do not receive the number of layers -> halving (output nodes = input nodes / 2) of the nodes for each neural network layer starts
-            layer_list = nn.ModuleList()
+        layer_list = nn.ModuleList()
+        if n_layers == None:              # if we do not receive the number of layers -> halving (output nodes = input nodes / 2) of the nodes for each neural network layer starts
             in_nodes = input_dim
-            if (input_dim // 2) % 2 != 0:       # if we have odd number of input nodes, we turn it to even number and start halving
-                out_nodes = input_dim // 2 + 1
+            if input_dim == 0:                                  # we cannot have dimension = 0
+                raise ValueError("Input dimension cannot be zero!!!")
+            elif input_dim == 1 or input_dim == 2 or input_dim == 3:   # if input dimension = 1 or 2 or 3 -> we create just 1 linear layer with output dimension = 2
+                out_nodes = 2
                 layer_list.append(nn.Linear(in_nodes, out_nodes))
                 layer_list.append(nn.ReLU())
-                in_nodes = out_nodes
-            else:                                     # if we have even number of input nodes, we start halving without any changes
-                out_nodes = input_dim // 2
-            while out_nodes % 2 == 0 and (in_nodes // 2) != 1 and out_nodes != 0:
+            while (in_nodes // 2) != 0 and (in_nodes // 2) != 1 and in_nodes != 1:               # we create layers with output nodes twice less than input nodes until we reach
                 out_nodes = in_nodes//2
                 layer_list.append(nn.Linear(in_nodes, out_nodes))
                 layer_list.append(nn.ReLU())
@@ -32,7 +32,6 @@ class MLP(nn.Module):
             self.layers = nn.Sequential(*layer_list)
             self.out_nodes = out_nodes
         else:                                 # if we receive the number of layers -> we linearly decrease number of nodes for each neural network layer
-            layer_list = nn.ModuleList()
             nodes = np.linspace(input_dim, 1, num=n_layers + 2, dtype=int)
             for idx in range(len(nodes) - 2):
                 nodes[idx], nodes[idx + 1]
@@ -46,12 +45,19 @@ class MLP(nn.Module):
         x = self.layers(x.float())
         if self.fusion == "intermediate":       # for intermediate fusion we return extracted features before the output layer
             return x
-        if self.fusion == "early" or self.fusion ==  "late":        # for early and late fusion we return MLP model with output linear layer
-            output_fc = nn.Linear(self.out_nodes, self.output_dim)
+        elif self.fusion == "early" or self.fusion ==  "late":        # for early and late fusion we return MLP model with output linear layer
+            output_fc = nn.Linear(self.out_nodes, self.output_dim).to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
             y_pred = output_fc(x)
-            return y_pred
+            if self.mode == "classification":       # for classification we need to add sigmoid activation function to the output of linear layer
+                sigmoid = nn.Sigmoid().to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
+                sigmoid_output = sigmoid(y_pred)
+                return sigmoid_output
+            elif self.mode == "regression":         # for regression we just return the output of linear layer
+                return y_pred
+            else:
+                raise ValueError("Incorrect model type selected!!!")
         else:
-            raise ValueError("Incorrect fusion mode!!!")
+            raise ValueError("Incorrect fusion selected!!!")
 
 def train_one_epoch(model, optimizer, train_loader, criterion, device):
   model.train()
@@ -59,8 +65,8 @@ def train_one_epoch(model, optimizer, train_loader, criterion, device):
   for inputs, labels in train_loader:
     inputs = inputs.to(device)
     labels = labels.to(device)
-    outputs = model(inputs)                        #Forward propagation
-    loss = criterion(outputs, labels.unsqueeze(1)) #Compute loss function
+    outputs = model(inputs)                                   #Forward propagation
+    loss = criterion(outputs, labels.float().unsqueeze(1))    #Compute loss function
     optimizer.zero_grad()
     loss.backward()                                #Backward propagation
     optimizer.step()                               #Update parameters
@@ -75,8 +81,8 @@ def validate(model, val_loader, criterion, device):
     for inputs, labels in val_loader:
       inputs = inputs.to(device)
       labels = labels.to(device)
-      outputs = model(inputs)                                 #Forward propagation
-      val_loss = criterion(outputs, labels.unsqueeze(1))      #Compute loss function
+      outputs = model(inputs)                                         #Forward propagation
+      val_loss = criterion(outputs, labels.float().unsqueeze(1))      #Compute loss function
       loss_step.append(val_loss.item())
   val_loss_epoch = torch.tensor(loss_step).mean().numpy()
   return val_loss_epoch
@@ -97,7 +103,7 @@ def load_model(model, path):
 def train(model, optimizer, num_epochs, train_loader, val_loader, criterion, device, path):
   dict_log = {"train_loss": [], "val_loss": []}
   best_val_loss = float("inf")
-  model = model.to(device)
+  # model = model.to(device)
   for epoch in range(num_epochs):
     loss_curr_epoch = train_one_epoch(model, optimizer, train_loader, criterion, device=device)       #train for one epoch
     val_loss = validate(model, val_loader, criterion, device)                                         #validate in the same epoch
@@ -112,9 +118,10 @@ def train(model, optimizer, num_epochs, train_loader, val_loader, criterion, dev
   return dict_log
 
 #----------------------------------------------------Early fusion---------------------------------------------------------------
-def early_fusion(dimension_dict, loaders_dict, device, lr=0.01, num_epochs=1, criterion=nn.L1Loss()):
+def early_fusion(dimension_dict, loaders_dict, device, lr, num_epochs, mode, criterion):
     input_dim = sum(list(dimension_dict.values()))
-    model = MLP(input_dim=input_dim, fusion="early")
+    model = MLP(input_dim=input_dim, fusion="early", mode=mode)
+    model = model.to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     model_path = 'best_early_fusion_model.pth'
     #train model with validation
